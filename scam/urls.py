acc7b20402d11113df798aaada0ed37dd79541b5
Obeid Arash <obeid75arash@gmail.com>
2022-08-08 05:43:41 -0700

    first commit, start custom user app and base of the project

---
+++ scam/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('users.urls', namespace='users'))
+]
+
+admin.site.site_header = "Admin Area"
+admin.site.site_title = "Title"
+admin.site.index_title = "Welcome to Admin Area"
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
