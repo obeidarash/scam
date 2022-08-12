c02fc7bf26a628532812b4754fb027385c3fcec5
Obeid Arash <obeid75arash@gmail.com>
2022-08-12 23:04:45 +0430

    install tinymce package

--- scam/settings.py
+++ scam/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'users',
     'financial',
     'widget_tweaks',
+    'tinymce',
     'access_token',
 ]
 
--- scam/urls.py
+++ scam/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),
     path('', index, name='home'),
     path('manifest', manifest, name='manifest'),
     path('', include('users.urls', namespace='users')),
