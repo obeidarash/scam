bbab31a8df5279476b208764ef43091f4c9b026a
Obeid Arash <obeid75arash@gmail.com>
2022-08-22 17:38:35 +0430

    fix bugs in users model

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django_countries.fields import CountryField
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-# this project made based on this tutorial
+# this custom user model made based on this tutorial
 # https://www.youtube.com/watch?v=SFarxlTzVX4&list=PLeg9XWX-QxbwLvK8O_xIG6zaCrON-Frjq&index=6
 
 
@@ -30,7 +30,7 @@ class UserManager(BaseUserManager):
         user = self.create_user(
             email=self.normalize_email(email),
             username=username,
-            password=password
+            password=password,
         )
         user.is_admin = True
         user.is_staff = True
@@ -42,7 +42,7 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser):
     email = models.EmailField(verbose_name='Email', max_length=60, unique=True)
     username = models.CharField(max_length=30, unique=True)
-    fullname = models.CharField(max_length=30, null=True)
+    fullname = models.CharField(max_length=30)
     country = CountryField(null=True)
     phone_number = PhoneNumberField(unique=True)
     date_joined = models.DateTimeField(verbose_name='Date Joined', auto_now_add=True)
@@ -59,7 +59,7 @@ class User(AbstractBaseUser):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['username', ]
 
     def __str__(self):
         return self.email
