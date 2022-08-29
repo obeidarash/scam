9ba00b7d837ef9241f23ebb114b42e58be80744a
Obeid Arash <obeid75arash@gmail.com>
2022-08-29 08:29:52 +0430

    fix bug of index.html

--- core/models.py
+++ core/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from tinymce.models import HTMLField
 
 
+# todo: creat a Q&A model
+
+
+
 class Contact(models.Model):
     title = models.CharField(max_length=512)
     content = HTMLField()
