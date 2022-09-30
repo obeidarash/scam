c409f8f31d2d49a2cb1a1f556a86c3905cc2fdd3
Obeid Arash <obeid75arash@gmail.com>
2022-10-01 01:41:14 +0330

    change mysql to sqlite in production

--- settings/production.py
+++ settings/production.py
@@ -6,14 +6,21 @@ ALLOWED_HOSTS = ['obeidarash.ir', 'www.obeidarash.ir']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'obeidara_thisisatest',  # database name
-        'USER': 'obeidara_testuser',
-        'PASSWORD': 'auLE&mu~Zd.W',
-        'HOST': 'obeidarash.ir',
-        'PORT': '3306',
-        'OPTIONS': {
-            'autocommit': True
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'mysql.connector.django',
+#         'NAME': 'obeidara_thisisatest',  # database name
+#         'USER': 'obeidara_testuser',
+#         'PASSWORD': 'auLE&mu~Zd.W',
+#         'HOST': 'obeidarash.ir',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'autocommit': True
+#         }
+#     }
+# }
