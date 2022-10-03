96b33c8280cd338edb6f1742b38eaca883a2f838
Obeid Arash <obeid75arash@gmail.com>
2022-10-03 13:02:15 +0330

    before deply on new host

--- settings/development.py
+++ settings/development.py
@@ -10,3 +10,18 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'mysql.connector.django',
+#         'NAME': 'obeidara_thisisatest',  # database name
+#         'USER': 'obeidara_testuser',
+#         'PASSWORD': 'auLE&mu~Zd.W',
+#         'HOST': 'obeidarash.ir',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'autocommit': True
+#         }
+#     }
+# }
--- settings/production.py
+++ settings/production.py
@@ -2,25 +2,25 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['obeidarash.ir', 'www.obeidarash.ir']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+ALLOWED_HOSTS = ['isitnowruz.ir', 'www.isitnowruz.ir']
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'mysql.connector.django',
-#         'NAME': 'obeidara_thisisatest',  # database name
-#         'USER': 'obeidara_testuser',
-#         'PASSWORD': 'auLE&mu~Zd.W',
-#         'HOST': 'obeidarash.ir',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'autocommit': True
-#         }
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'obeidara_thisisatest',  # database name
+        'USER': 'obeidara_testuser',
+        'PASSWORD': 'auLE&mu~Zd.W',
+        'HOST': 'obeidarash.ir',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
+    }
+}
