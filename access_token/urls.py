9ca4723dc01e8653c819578c8f53fe1cf35e4225
Obeid Arash <obeid75arash@gmail.com>
2022-10-06 03:45:32 +0330

    change token list pgae url and fix the col

--- access_token/urls.py
+++ access_token/urls.py
@@ -3,6 +3,6 @@ from .views import at, at_generator
 
 app_name = 'at'
 urlpatterns = [
-    path('at', at, name='at'),
-    path('at_generator', at_generator, name='at_generator'),
+    path('a', at, name='at'),
+    path('a_generator', at_generator, name='at_generator'),
 ]
