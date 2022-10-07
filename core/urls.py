44f9e59ad3ae46de9f9cebb437fae77482373e41
Obeid Arash <obeid75arash@gmail.com>
2022-10-08 02:58:02 +0330

    add tabs to commit!

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import manifest, contact
 
+# todo: add dbbackup to this app
+# use this youtube tutorial https://www.youtube.com/watch?v=s54HYoJ8wrs&list=PLeg9XWX-QxbwLvK8O_xIG6zaCrON-Frjq&index=1
+# add a 'get backup' button and a cronjob
+
 app_name = 'core'
 
 urlpatterns = [
