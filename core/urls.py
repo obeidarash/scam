2177d425e11ed4f0433c1bbb0ad40bc61cdfb407
Obeid Arash <obeid75arash@gmail.com>
2022-10-18 17:06:39 +0330

    add custom 404 and 500 page error - basic

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import manifest, contact, Blog, PostDetail
+from django.conf.urls import handler404, handler500
 
 # todo: add dbbackup to this app
 # use this youtube tutorial https://www.youtube.com/watch?v=s54HYoJ8wrs&list=PLeg9XWX-QxbwLvK8O_xIG6zaCrON-Frjq&index=1
@@ -13,3 +14,5 @@ urlpatterns = [
     path('blog', Blog.as_view(), name='blog'),
     path('post/<slug:slug>', PostDetail.as_view(), name='post'),
 ]
+
+handler404 = 'core.views.error_404'
--- core/views.py
+++ core/views.py
@@ -40,3 +40,11 @@ def manifest(request):
         'contact': contact,
     }
     return render(request, 'manifest.html', context)
+
+
+def error_404(request, exception):
+    return render(request, 'core/404.html', status=404)
+
+
+def error_500(request, exception):
+    return render(request, 'core/500.html', status=500)
