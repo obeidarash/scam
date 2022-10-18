8976e1c665706e1274a7443e49bafc69a661dee0
Obeid Arash <obeid75arash@gmail.com>
2022-10-18 17:10:38 +0330

    take of 500 error handler

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import manifest, contact, Blog, PostDetail
-from django.conf.urls import handler404, handler500
+from django.conf.urls import handler404
 
 # todo: add dbbackup to this app
 # use this youtube tutorial https://www.youtube.com/watch?v=s54HYoJ8wrs&list=PLeg9XWX-QxbwLvK8O_xIG6zaCrON-Frjq&index=1
@@ -16,4 +16,4 @@ urlpatterns = [
 ]
 
 handler404 = 'core.views.error_404'
-handler500 = 'core.views.error_500'
+
